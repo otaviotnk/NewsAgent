6aea4d3e739cb0cb5515af2281ab753a6cb05886
Otavio <otavio.atr@gmail.com>
2026-04-06 09:00:11 -0300

    add controller

--- NewsAgent/Controllers/NewsAgentController.cs
+++ NewsAgent/Controllers/NewsAgentController.cs
@@ -1,6 +1,31 @@
-﻿namespace NewsAgent.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsAgent.Agent;
+using NewsAgent.Models;
+
+namespace NewsAgent.Controllers
 {
-    public class NewsAgentController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NewsAgentController : ControllerBase
     {
+        private readonly NewsAgentService _agentService;
+
+        public NewsAgentController(NewsAgentService agentService)
+        {
+            _agentService = agentService;
+        }
+
+        [HttpPost("ask")]
+        [ProducesResponseType(typeof(AgentResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Ask([FromBody] AgentRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Question))
+                return BadRequest("A pergunta não pode ser vazia.");
+
+            var answer = await _agentService.AskAsync(request.Question);
+
+            return Ok(new AgentResponse(request.Question, answer));
+        }
     }
 }
